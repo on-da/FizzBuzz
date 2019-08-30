86ee6d6e5b68fb9d2dd7c84d9cc304c7eb6f1127
on-da <53707132+on-da@users.noreply.github.com>
2019-08-30 15:43:20 +0900

    ボタンを押したらランダムに計算上限が算出されて表示される形式に変更

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,31 +1,38 @@
-//関数作成
+
+
+//gameStart関数の作成
 function gameStart(){
-//入力要素の取得
-    let inputFizzBuzz=document.getElementById("inputFizzBuzz");
+    //上限値を生成
+    let randomLimit=Math.floor(Math.random()*(100-1))+1;
+    //空の配列を作成
+    let fizzbuzzNumber=[];
+
+//結果をHTMLに反映させる
+let fizzbuzzResult=document.createElement("li");
+fizzbuzzResult.innerHTML=fizzbuzzNumber.join("\n");
+
+document.getElementById("results").appendChild(fizzbuzzResult);
     
-    //FizzBuzzの処理　3で割り切れるFizz 5で割り切れるBuzz 3と5で割り切れるFizzBuzz
-    
-    for(let i=1;i<=inputFizzBuzz;i++){
-    if(i/3===0){
-        console.log("Fizz!");
-    }else if(i/5===0){
-        console.log("Buzz!");
-    }else if(i/15===0){
-        console.log("FizzBuzz!");
-    }else{
+        
+   //for if文でFizzBuzzの処理開始
+   for(let i=1;i<=randomLimit;i++){
+       if(i%3==0 && i%5==00){
+       console.log("fizzbuzz");
+       }else if(i%3==0){
+        console.log("fizz");    
+       }else if(i%5==0){
+        console.log("buzz");
+       }else{
         console.log(i);
-    } 
-    }
+   }
+};
+    return fizzbuzzNumber;
 
-    let FizzBuzz=document.createElement("li");
-    FizzBuzz.innerHTML=gameStart().value
-
-    document.getElementById("result").appendChild(FizzBuzz);
+};
 
 
-}
 
-    
+
     
 
 
